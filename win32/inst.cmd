24a15a0ec49a9e6dc99dfa15baae79dac78d3083
Frank M. Kromann <fmk@php.net>
2001-02-27 17:53:20 +0000

    Adding a simple install script

---
+++ win32/inst.cmd
@@ -0,0 +1,7 @@
+@echo off
+copy "..\..\src\gtk+\gtk\gtk-1.3.dll" %SystemRoot%\system32
+copy "..\..\src\gtk+\gdk\gdk-1.3.dll" %SystemRoot%\system32
+copy "..\..\src\glib\gmodule\gmodule-1.3.dll" %SystemRoot%\system32
+copy "..\..\src\glib\glib-1.3.dll" %SystemRoot%\system32
+copy "..\..\src\libiconv-1.3\src\iconv-1.3.dll" %SystemRoot%\system32
+copy "..\..\src\intl\gnu-intl.dll" %SystemRoot%\system32
