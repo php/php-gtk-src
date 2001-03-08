bbb06c9cbf0874d6bd69e43d5ac2e6860a337bbe
Andrei Zmievski <andrei@php.net>
2001-03-08 03:02:27 +0000

    Fixing after API changes.

--- test/fileselection.php
+++ test/fileselection.php
@@ -2,45 +2,44 @@
 
 /* $Id$ */
 
-if (!class_exists("gtk")) {
-	if (strtoupper(substr(PHP_OS, 0,3) == "WIN"))
-		dl("php_gtk.dll");
+if (!class_exists('gtk')) {
+	if (strtoupper(substr(PHP_OS, 0,3) == 'WIN'))
+		dl('php_gtk.dll');
 	else
-		dl("php_gtk.so");
+		dl('php_gtk.so');
 }
 
 function shutdown() {
 	print("Shutting down.\n");
 	Gtk::main_quit();
-	return true;
 }
 
 function EndDialog() {
-	print("ok_button clicked\n");
+	print("OK button clicked\n");
 	return true;
 }
 
 function CancelDialog() {
-	print("cansel_button clicked\n");
+	print("Cancel button clicked\n");
 	return true;
 }
 
 function FileDialog() {
-	$fs = &new GtkFileSelection("Save Result to file");
+	$fs = &new GtkFileSelection('File Selection Example');
 	$ok_button = $fs->ok_button;
-	$ok_button->connect("clicked", "enddialog");
-	$ok_button->connect_object("clicked", "destroy", $fs);
+	$ok_button->connect('clicked', 'enddialog');
+	$ok_button->connect_object('clicked', array($fs, 'destroy'));
 	$cancel_button = $fs->cancel_button;
-	$cancel_button->connect("clicked", "canceldialog");
-	$cancel_button->connect_object("clicked", "destroy", $fs);
+	$cancel_button->connect('clicked', 'canceldialog');
+	$cancel_button->connect_object('clicked', array($fs, 'destroy'));
 	$fs->show();
 }
 
-$window = &new GtkWindow(GDK_WINDOW_TOPLEVEL);
-$window->connect("destroy", "shutdown");
+$window = &new GtkWindow();
+$window->connect('destroy', 'shutdown');
 
-$button = &new GtkButton("Open File Dialog");
-$button->connect("clicked", "FileDialog");
+$button = &new GtkButton('Open File Dialog');
+$button->connect('clicked', 'FileDialog');
 $window->add($button);
 
 $window->show_all();
