af7fa15aecdb77e00f9483d1d55ec52d39d8e251
Frank M. Kromann <fmk@php.net>
2001-03-13 07:59:42 +0000

    Adding missing prototypes

--- main/php_gtk.h
+++ main/php_gtk.h
@@ -110,6 +110,9 @@ zval php_gtk_get_property(zend_property_reference *property_reference);
 int php_gtk_set_property(zend_property_reference *property_reference, zval *value);
 void php_gtk_call_function(INTERNAL_FUNCTION_PARAMETERS, zend_property_reference *property_reference);
 
+void php_gdk_register_classes(void);
+void php_gdk_register_constants(int module_number ELS_DC);
+
 static inline void php_gtk_register_prop_getter(zend_class_entry *ce, prop_getter_t getter)
 {
 	zend_hash_index_update(&php_gtk_prop_getters, (long)ce, (void*)&getter,
