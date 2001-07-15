3074d61ce91c3552c9ed3f96cbf26538c435d181
Frank M. Kromann <fmk@php.net>
2001-07-15 22:51:47 +0000

    Adding test for environment variable PHP_BIN. Adding error message when no script is passed as first parameter

--- win32/php_win.c
+++ win32/php_win.c
@@ -1,16 +1,21 @@
 #include <windows.h>
 #include <process.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int WINAPI WinMain(
-  HINSTANCE hInstance,  // handle to current instance
-  HINSTANCE hPrevInstance,  // handle to previous instance
-  LPSTR lpCmdLine,      // pointer to command line
-  int nCmdShow          // show state of window
+	HINSTANCE hInstance,		// handle to current instance
+	HINSTANCE hPrevInstance,	// handle to previous instance
+	LPSTR lpCmdLine,			// pointer to command line
+	int nCmdShow				// show state of window
 ) 
 {
 	char *args[4], *prog;
 
-	prog = "c:\\php4\\php.exe";
+	if (strlen(lpCmdLine) == 0)
+		MessageBox(NULL, "Usage: php_win <path to php-gtk script>\n", "Error", MB_OK);
+	if (!(prog = getenv("PHP_BIN")))
+		prog = "c:\\php4\\php.exe";
 	args[0] = prog;
 	args[1] = "-q";
 	args[2] = lpCmdLine;
