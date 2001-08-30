c77f33aea6da8f6c64d79eb7ffd2bdc936966cd1
Frank M. Kromann <fmk@php.net>
2001-08-30 21:12:02 +0000

    Adding a "nice" error message if PHP.EXE was not found

--- win32/php_win.c
+++ win32/php_win.c
@@ -10,16 +10,32 @@ int WINAPI WinMain(
 	int nCmdShow				// show state of window
 ) 
 {
-	char *args[4], *prog;
+	char *args[4], *prog = NULL;
+	char pathbuffer[_MAX_PATH];
+	char searchfile[] = "php.exe";
+	char envvar[] = "PATH";
+	char *path = getenv("PATH");
+	char newpath[2048];
+
+	sprintf(newpath, "PATH=c:\\php4;d:\\php4;%s", path);
+	putenv(newpath);
 
 	if (strlen(lpCmdLine) == 0)
 		MessageBox(NULL, "Usage: php_win <path to php-gtk script>\n", "Error", MB_OK);
-	if (!(prog = getenv("PHP_BIN")))
-		prog = "c:\\php4\\php.exe";
+	if (!(prog = getenv("PHP_BIN"))) {
+		_searchenv( searchfile, envvar, pathbuffer );
+		if (strlen(pathbuffer))
+			prog = pathbuffer;
+	}
 	args[0] = prog;
 	args[1] = "-q";
 	args[2] = lpCmdLine;
 	args[3] = NULL;
 
-	return _spawnvp(_P_DETACH, prog, args);
+	if (prog)
+		return _spawnvp(_P_DETACH, prog, args);
+	else {
+		MessageBox(NULL, "PHP.EXE was not found.", "Error", MB_OK);
+		return -1;
+	}
 }
