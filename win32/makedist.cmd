cad7bc9b10c56655c5049f9d0849da5c6f66677d
Frank M. Kromann <fmk@php.net>
2001-11-11 19:22:59 +0000

    Rename samples to test according to the docs.

--- win32/makedist.cmd
+++ win32/makedist.cmd
@@ -4,7 +4,7 @@ cd \php-gtk-dist
 md php4
 md winnt
 md winnt\system32
-md samples
+md test
 copy \php4\php.exe php4
 copy \php4\php_win.exe php4
 copy \php4\php4ts.dll php4
@@ -19,13 +19,13 @@ copy %SystemRoot%\system32\iconv-1.3.dll winnt\system32
 copy %SystemRoot%\system32\gnu-intl.dll winnt\system32
 copy %SystemRoot%\system32\libglade-0.14.dll winnt\system32
 copy %SystemRoot%\system32\libxml.dll winnt\system32
-copy \php\php-gtk\test\fileselection.php samples
-copy \php\php-gtk\test\gtk.php samples
-copy \php\php-gtk\test\hello.php samples
-copy \php\php-gtk\test\list.php samples
-copy \php\php-gtk\test\scribble.php samples
-copy \php\php-gtk\test\testgtkrc samples
-copy \php\php-gtk\test\testgtkrc2 samples
+copy \php\php-gtk\test\fileselection.php test
+copy \php\php-gtk\test\gtk.php test
+copy \php\php-gtk\test\hello.php test
+copy \php\php-gtk\test\list.php test
+copy \php\php-gtk\test\scribble.php test
+copy \php\php-gtk\test\testgtkrc test
+copy \php\php-gtk\test\testgtkrc2 test
 copy \php\php-gtk\win32\README.txt 
 "c:\Program Files\WinZip\WZZip" -ampr php-gtk-0.1.1a-win32.zip
 cd \php\php-gtk\win32
