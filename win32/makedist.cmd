4d4bace640a21377bcb4bbe86999c005252157c1
Frank M. Kromann <fmk@php.net>
2002-11-23 19:59:17 +0000

    Updating script to include new GTK+ dll's Adding extra samples and removing php4isapi.dll

--- win32/makedist.cmd
+++ win32/makedist.cmd
@@ -10,28 +10,34 @@ copy \php4\php.exe php4
 copy \php4\php-cgi.exe php4
 copy \php4\php_win.exe php4
 copy \php4\php4ts.dll php4
-copy \php4\php4isapi.dll php4
 copy \php4\php_gtk.dll php4
 copy \php4\php_gtk_libglade.dll php4
 copy \php4\php_gtk_sqpane.dll php4
 copy \php4\php_gtk_combobutton.dll php4
 copy \php\php4\php.ini-dist winnt\php.ini
 type \php\php-gtk\win32\php.ini >>winnt\php.ini
-copy %SystemRoot%\system32\gtk-1.3.dll winnt\system32
-copy %SystemRoot%\system32\gdk-1.3.dll winnt\system32
-copy %SystemRoot%\system32\gmodule-1.3.dll winnt\system32
-copy %SystemRoot%\system32\glib-1.3.dll winnt\system32
-copy %SystemRoot%\system32\iconv-1.3.dll winnt\system32
-copy %SystemRoot%\system32\gnu-intl.dll winnt\system32
-copy %SystemRoot%\system32\libglade-0.14.dll winnt\system32
-copy %SystemRoot%\system32\libxml.dll winnt\system32
+
+copy \php4\iconv.dll php4
+copy \php4\libgdk-0.dll php4
+copy \php4\libglade-0.1.dll php4
+copy \php4\libglib-2.0-0.dll php4
+copy \php4\libgmodule-2.0-0.dll php4
+copy \php4\libgobject-2.0-0.dll php4
+copy \php4\libgthread-2.0-0.dll php4
+copy \php4\libgtk-0.dll php4
+copy \php4\libintl-1.dll php4
+copy \php4\libxml2.dll php4
+
+copy \php\php-gtk\test\combobutton.php test
 copy \php\php-gtk\test\fileselection.php test
 copy \php\php-gtk\test\gtk.php test
 copy \php\php-gtk\test\hello.php test
 copy \php\php-gtk\test\list.php test
 copy \php\php-gtk\test\scribble.php test
+copy \php\php-gtk\test\sqpane.php test
 copy \php\php-gtk\test\testgtkrc test
 copy \php\php-gtk\test\testgtkrc2 test
+copy \php\php-gtk\test\window.xpm test
 copy \php\php-gtk\win32\README.txt 
 copy \php\php-gtk\NEWS
 "c:\Program Files\WinZip\WZZip" -ampr %zip_file% 
