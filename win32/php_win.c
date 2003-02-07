749b5c6840851deb3d5e44f0a78cc2e739dc1434
Frank M. Kromann <fmk@php.net>
2003-02-07 00:12:42 +0000

    Extend search to look for php.exe in the same directory where php_win.exe is located

--- win32/php_win.c
+++ win32/php_win.c
@@ -2,6 +2,8 @@
 #include <process.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int WINAPI WinMain(
 	HINSTANCE hInstance,		// handle to current instance
@@ -11,26 +13,45 @@ int WINAPI WinMain(
 ) 
 {
 	char *args[4], *prog = NULL;
-	char pathbuffer[_MAX_PATH];
-	char searchfile[] = "php.exe";
-	char envvar[] = "PATH";
-	char *path = getenv("PATH");
-	char newpath[2048];
+	char binary_file[] = "php.exe";
+	char modulepath[_MAX_PATH];
 
-	sprintf(newpath, "PATH=c:\\php4;d:\\php4;%s", path);
-	putenv(newpath);
-
-	if (strlen(lpCmdLine) == 0)
-		MessageBox(NULL, "Usage: php_win <path to php-gtk script>\n", "Error", MB_OK);
-	if (!(prog = getenv("PHP_BIN"))) {
-		_searchenv( searchfile, envvar, pathbuffer );
-		if (strlen(pathbuffer))
-			prog = pathbuffer;
+	// Look for php.exe in the same directory as php_win.exe
+	if (GetModuleFileName(NULL, modulepath, _MAX_PATH)) {
+		char *separator_location = strrchr(modulepath, '\\');
+		if (separator_location) {
+			struct stat statbuf;
+			strcpy(separator_location+1, binary_file);
+			if (stat(modulepath, &statbuf) == 0) {
+				if (((statbuf.st_mode & S_IEXEC) == S_IEXEC)) {
+					prog = modulepath;
+				}
+			}
+		}
 	}
+
+	// Search the path for the file
+	if (!prog) {
+		char pathbuffer[_MAX_PATH];
+		char envvar[] = "PATH";
+		char *path = getenv("PATH");
+		char newpath[2048];
+		sprintf(newpath, "PATH=.;c:\\php4;d:\\php4;%s", path);
+		putenv(newpath);
+
+		if (strlen(lpCmdLine) == 0)
+			MessageBox(NULL, "Usage: php_win <path to php-gtk script>\n", "Error", MB_OK);
+		if (!(prog = getenv("PHP_BIN"))) {
+			_searchenv( binary_file, envvar, pathbuffer );
+			if (strlen(pathbuffer))
+				prog = pathbuffer;
+		}
+	}
+
+	// Set program parameters
 	args[0] = prog;
-	args[1] = "-q";
-	args[2] = lpCmdLine;
-	args[3] = NULL;
+	args[1] = lpCmdLine;
+	args[2] = NULL;
 
 	if (prog)
 		return _spawnvp(_P_DETACH, prog, args);
