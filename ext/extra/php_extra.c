b4c4c328d56393cfd67f70900ce65326a0c90586
SVN Migration <svn@php.net>
2003-06-28 14:47:13 +0000

    This commit was manufactured by cvs2svn to create branch 'ZE2_PORT'.

---
+++ ext/extra/php_extra.c
@@ -0,0 +1,45 @@
+/*
+ * PHP-GTK - The PHP language bindings for GTK+
+ *
+ * Copyright (C) 2001,2002 Andrei Zmievski <andrei@php.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+/* $Id$ */
+
+#include "php_extra.h"
+
+#if HAVE_EXTRA
+
+#ifdef PHP_GTK_COMPILE_DL_EXTRA
+PHP_GTK_GET_EXTENSION(extra)
+#endif
+
+PHP_GTK_XINIT_FUNCTION(extra)
+{
+	php_extra_register_constants(module_number TSRMLS_CC);
+	php_extra_register_classes();
+
+	return SUCCESS;
+}
+
+php_gtk_ext_entry extra_ext_entry = {
+	"extra",
+	PHP_GTK_XINIT(extra),
+	NULL,
+};
+
+#endif	/* HAVE_EXTRA */
