5348a668b113b4eba0b886a794522029aace3ebb
Alan Knowles <alan_k@php.net>
2003-08-21 15:06:23 +0000

    add the one and only example of callback registry and php_gtk_simple_signal_callback

--- ext/extra/sheet.php
+++ ext/extra/sheet.php
@@ -1,4 +1,8 @@
 <?php
+function test_traverse() {
+	print_r( func_get_args());
+}
+error_reporting('E_ALL');
 
 if (!extension_loaded('gtk')) {
 	dl( 'php_gtk.' . PHP_SHLIB_SUFFIX);
@@ -10,8 +14,11 @@ $window->set_position(GTK_WIN_POS_CENTER);
 $window->set_default_size(gdk::screen_width(), gdk::screen_height()-20);
 
 $scrolledwindow = &new GtkScrolledWindow();
-$sheet   = &new GtkSheet(101,101, 'test');
-$scrolledwindow->add($sheet);
+
+$sheet   = &new GtkSheet(100,100,"test"); 
+$sheet->connect('traverse','test_traverse');
+
+$scrolledwindow->add($sheet); 
 
 $window->add($scrolledwindow);
 $window->show_all();
