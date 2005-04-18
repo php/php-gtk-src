487219e3df7e17ede1c2c9db6c7688c5441582e6
foobar <sniper@php.net>
2005-04-18 10:48:31 +0000

    - Added cvsclean script - Fixed buildconf with new phpize which does not run aclocal anymore # The problem is actually with multiple versions of auto* tools. # Best way to fix that once and for all is to get totally rid of the # aclocal requirement. :) # There is a reason why all automake crap was removed in PHP..

--- build2/build.mk
+++ build2/build.mk
@@ -8,12 +8,20 @@ STAMP = buildmk.stamp
 
 ALWAYS = build2/generated_lists
 
-all: $(ALWAYS) $(STAMP)
+all: $(STAMP) $(ALWAYS)
 	@$(MAKE) -s -f build2/build2.mk
 
 build2/generated_lists:
-	@echo config_m4_files = `build2/config-stubs ext ext.m4` >> $@
+	@echo config_m4_files = `build2/config-stubs ext ext.m4` > $@
 
-$(STAMP):
+$(STAMP): 
+	touch ext.m4
 	$(PHPIZE)
 	touch $(STAMP)
+
+cvsclean-work:
+	@for i in `find . -name .cvsignore`; do \
+		(cd `dirname $$i` 2>/dev/null && rm -rf `cat .cvsignore | grep -v config.nice` *.o *.a .libs || true); \
+	done
+
+.PHONY: $(ALWAYS)
--- build2/build2.mk
+++ build2/build2.mk
@@ -6,13 +6,18 @@ include build2/generated_lists
 all: configure config.h.in
 
 ext.m4: $(config_m4_files)
+	@echo rebuilding $@
 	@cat $(config_m4_files) > $@
 
-configure: aclocal.m4 config.m4 php_gtk.m4 ext.m4
+aclocal.m4: configure.in acinclude.m4
 	@echo rebuilding $@
 	@aclocal
+	
+configure: aclocal.m4 config.m4 php_gtk.m4 ext.m4
+	@echo rebuilding $@
 	@autoconf 
-
+	@chmod 755 $@
+	
 config.h.in: configure
 # explicitly remove target since autoheader does not seem to work 
 # correctly otherwise (timestamps are not updated)
