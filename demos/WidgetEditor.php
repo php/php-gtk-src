97cbae44c861b23be3e363e110c312af0185f9fa
Christian Weiske <cweiske@php.net>
2005-07-21 07:26:58 +0000

    Small demo which lists all Stock items in a GtkIconView. And there is a small "Widget editor" which lets you edit some Values of the iconview directly when running the demo.

---
+++ demos/WidgetEditor.php
@@ -0,0 +1,120 @@
+<?php
+/**
+*   Small window which displays various controls
+*   to change settings of a given widget.
+*   To see how the widget behaves
+*
+*   @author Christian Weiske <cweiske@php.net>
+*/
+
+
+class WidgetEditor extends GtkWindow
+{
+    private $table = null;
+    
+    
+    function __construct($widget, $data)
+    {
+        parent::__construct();
+        $this->set_type_hint(Gdk::WINDOW_TYPE_HINT_UTILITY);
+        $this->set_title('WidgetEditor');
+        
+        $this->table = new GtkTable(count($data), 2, false);
+        
+        foreach ($data as $nPos => $dat) {
+            $this->addEditor($widget, $nPos, $dat);
+        }
+        
+        $this->add($this->table);
+        
+        
+        $this->show_all();
+    }//function __construct($widget, $data)
+
+
+
+    function addEditor($basewidget, $position, $data)
+    {
+        $this->table->attach(new GtkLabel($data[0]), 0, 1, $position, $position + 1);
+
+        switch ($data[1]) {
+            case GtkScale::gtype:
+                $widget = $this->createScale($basewidget, $data[0], $data[2], $data[3]);
+                break;
+            case GtkComboBox::gtype:
+                $widget = $this->createComboBox($basewidget, $data[0], $data[2]);
+                break;
+            default:
+                $widget = new GtkLabel('Unsupported type ' . $data[1]);
+                break;
+        }
+
+        $this->table->attach($widget, 1, 2, $position, $position + 1);
+    }//function addEditor($position, $data)
+
+
+
+    function createScale($basewidget, $basefunction, $from, $to)
+    {
+        $scale = GtkHScale::new_with_range($from, $to, 1);
+        
+        $function = 'get_' . $basefunction;
+        $scale->set_value($basewidget->$function());
+        
+        $scale->connect('change-value', array($this, 'set_scale'), $basewidget, 'set_' . $basefunction);
+        
+        return $scale;
+    }//function createScale($basewidget, $function, $from, $to)
+
+
+
+    function createComboBox($basewidget, $basefunction, $strConstants)
+    {
+        $combo = GtkComboBox::new_text();
+        $type = substr($strConstants, 0, strpos($strConstants, '::'));
+        $name = substr($strConstants, strpos($strConstants, '::') + 2);
+
+        $r = new ReflectionClass($type);
+        $arConstants = $r->getConstants();
+
+        foreach ($arConstants as $id => $value) {
+            if (substr($id, 0, strlen($name)) == $name) {
+                $combo->insert_text($value, $type . '::' . $id);
+            }
+        }
+        
+        $function = 'get_' . $basefunction;
+        $combo->set_active($basewidget->$function());
+        $combo->connect('changed', array($this, 'set_combobox'), $basewidget, 'set_' . $basefunction);
+
+        return $combo;
+    }//function createComboBox($basewidget, $basefunction, $strConstants)
+
+
+
+    function set_scale($scale, $type, $value, $widget, $function)
+    {
+        //I check it here because an out-of-bounds can occur while dragging the scale with the mouse
+        $adjustment = $scale->get_adjustment();
+        if ($value < $adjustment->lower) {
+            $value = $adjustment->lower;
+        } else if ($value > $adjustment->upper) {
+            $value = $adjustment->upper;
+        }
+        
+        $widget->$function($value);
+    }//function set_scale($type, $value, $widget, $function)
+
+
+
+    function set_combobox($combo, $widget, $function)
+    {
+        $value = $combo->get_active();
+        $widget->$function($value);
+    }
+
+
+}//class WidgetEditor extends GtkWindow
+
+
+?>
