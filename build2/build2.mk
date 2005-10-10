3ee388ff47a10c0e9547c92a43cca7025b38a3ff
Andrei Zmievski <andrei@php.net>
2005-10-10 18:34:39 +0000

    Re-generate configure when config.m4 changes.

--- build2/build2.mk
+++ build2/build2.mk
@@ -12,7 +12,7 @@ aclocal.m4: configure.in acinclude.m4
 	@echo rebuilding $@
 	cat acinclude.m4 ./build/libtool.m4 php_gtk.m4 > $@
 	
-configure: aclocal.m4 php_gtk.m4 $(config_m4_files)
+configure: aclocal.m4 php_gtk.m4 config.m4 $(config_m4_files)
 	@echo rebuilding $@
 	$(PHP_AUTOCONF)
 	@chmod 755 $@
