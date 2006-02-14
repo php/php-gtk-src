421d25245b241a4b657f59a9cb7436bbaee9d19f
Andrei Zmievski <andrei@php.net>
2006-02-14 03:10:24 +0000

    Initialize free_* variable to FALSE.

--- generator/arg_types.php
+++ generator/arg_types.php
@@ -163,7 +163,7 @@ class String_Arg extends Arg_Type {
             $info->var_list->add('char', '*' . $name . ' = ' . $default);
         } else
             $info->var_list->add('char', '*' . $name);
-        $info->var_list->add('zend_bool', 'free_' . $name);
+        $info->var_list->add('zend_bool', 'free_' . $name . ' = FALSE');
         $info->add_parse_list('u', array("&$name", "&free_$name"));
         $info->arg_list[] = $name;
         $info->post_code[] = "\tif (free_$name) g_free($name);\n";
