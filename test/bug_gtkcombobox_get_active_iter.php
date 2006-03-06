d4eccfcc80573230836b3fd3a9fc6ef43629f185
Christian Weiske <cweiske@php.net>
2006-03-06 20:52:17 +0000

    Fixing get_active_iter() to not require a parameter but return one

---
+++ test/bug_gtkcombobox_get_active_iter.php
@@ -0,0 +1,14 @@
+<?php
+$cmb = new GtkComboBox();
+$mod = new GtkListStore(Gtk::TYPE_STRING);
+$cmb->set_model($mod);
+
+$mod->append(array("one"));
+$mod->append(array("two"));
+$mod->append(array("three"));
+
+$cmb->set_active(1);
+
+//that should return an iter, not require one
+var_dump($cmb->get_active_iter());
+?>
