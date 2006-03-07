9db097a5a29aee9ca6db3c1885de94a82db2fe1c
Christian Weiske <cweiske@php.net>
2006-03-07 12:36:30 +0000

    Added unittests generated by phpunit2. First methods will follow soon

---
+++ test/unittests/generator.php
@@ -0,0 +1,31 @@
+<?php
+// Use PHPUnit2 to create the skeletons.
+//ATTENTION: needs phpunit2 bugfix
+// http://pear.php.net/bugs/bug.php?id=6965
+require_once 'PHPUnit2/Util/Skeleton.php';
+
+// Get all of the classes in PHP-GTK 2.
+/* Get all classes and filter out Gtk classes only */
+// Taken from Anant's updater script.
+$classes = array();
+$allClasses = get_declared_classes();
+
+$extensions = array('Gtk', 'Gdk', 'Atk', 'Pango');
+foreach ($extensions as $extension) {
+    foreach($allClasses as $oneClass) {
+        if(strpos($oneClass, $extension) === 0) {
+            $classes[] = $oneClass;
+        }
+    }
+}
+
+sort($classes);
+
+foreach ($classes as $class) {
+    // Create a skeleton for the class.
+    $skeleton = new PHPUnit2_Util_Skeleton($class);
+
+    // Write the test.
+    $skeleton->write();
+}
+?>
