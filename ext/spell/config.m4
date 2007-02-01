d6deca99362a5e66f1d347efdce90eb94f328ffa
Anant Narayanan <anant@php.net>
2007-02-01 18:42:56 +0000

    Check in the GtkSpell extension.

---
+++ ext/spell/config.m4
@@ -0,0 +1,25 @@
+dnl $Id$
+dnl config.m4 for spell module
+
+define(spell_required_version,   2.0.0)
+
+PHP_GTK_ARG_ENABLE(spell,for spell support,
+[  --enable-spell      Enable spell support],no)
+
+if test "$PHP_GTK_SPELL" != "no"; then
+  PKG_CHECK_MODULES(SPELL, [gtkspell-2.0 >= spell_required_version],
+  have_spell=yes, have_spell=no)
+  if test "$have_spell" != "yes"; then
+    AC_MSG_RESULT([Unable to locate spell version spell_required_version or higher: not building])
+  else
+    AC_DEFINE(HAVE_SPELL, 1, [spell support])
+    PHP_EVAL_INCLINE($SPELL_CFLAGS)
+    if test "$php_gtk_ext_shared" = "yes"; then
+      PHP_EVAL_LIBLINE($SPELL_LIBS, SPELL_SHARED_LIBADD)
+      PHP_SUBST(SPELL_SHARED_LIBADD)
+    else
+      PHP_EVAL_LIBLINE($SPELL_LIBS, PHP_GTK2_SHARED_LIBADD)
+    fi
+    PHP_GTK_EXTENSION(spell, $php_gtk_ext_shared, php_spell.c, gen_spell.c)
+  fi
+fi
---
+++ ext/spell/php_spell.h
@@ -0,0 +1,44 @@
+/*
+ * PHP-GTK - The PHP language bindings for GTK+
+ *
+ * Copyright (C) 2001,2002 Andrei Zmievski <andrei@php.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
+/* $Id$ */
+
+#ifndef PHP_SPELL_H
+#define PHP_SPELL_H
+
+#include "php_gtk.h"
+
+#if HAVE_SPELL
+
+#include <gtkspell/gtkspell.h>
+
+extern php_gtk_ext_entry spell_ext_entry;
+#define php_gtk_ext_spell_ptr &spell_ext_entry
+
+void phpg_gtkspell_register_classes(void);
+void phpg_gtkspell_register_constants(const char *strip_prefix);
+
+#else
+
+#define php_gtk_ext_spell_ptr NULL
+
+#endif	/* HAVE_SPELL */
+
+#endif	/* PHP_SPELL_H */
