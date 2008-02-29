2546ce52ae8a845df7e04b00fd09f7e61e5ec17e
Andrei Zmievski <andrei@php.net>
2008-02-29 19:05:53 +0000

    Update Copyright year.

--- main/php_gtk_util.c
+++ main/php_gtk_util.c
@@ -1,7 +1,7 @@
 /*
  * PHP-GTK - The PHP language bindings for GTK+
  *
- * Copyright (C) 2001,2002,2003,2004 Andrei Zmievski <andrei@php.net>
+ * Copyright (C) 2001-2008 Andrei Zmievski <andrei@php.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
