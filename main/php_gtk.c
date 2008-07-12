ddd38be2f0fb53134a727f0469ce428c496c55ea
Elizabeth Marie Smith <auroraeosrose@php.net>
2008-07-12 23:52:25 +0000

    Fixed issue with zend_unset_timeout after conversation about the best way to handle disabling the timeout

--- main/php_gtk.c
+++ main/php_gtk.c
@@ -167,8 +167,7 @@ PHP_MSHUTDOWN_FUNCTION(gtk)
 
 PHP_RINIT_FUNCTION(gtk)
 {
-	zend_unset_timeout(TSRMLS_C);
-	zend_set_timeout(0);
+	zend_alter_ini_entry("max_execution_time", sizeof("max_execution_time")-1, "0", sizeof("0")-1,PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
 
 	php_gtk_handlers = *zend_get_std_object_handlers();
 	php_gtk_handlers.read_property  = phpg_read_property;
