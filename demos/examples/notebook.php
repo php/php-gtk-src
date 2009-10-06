f32269cf002f8e1f82e10bcff5715768a4c3a731
Elizabeth Marie Smith <auroraeosrose@php.net>
2009-10-06 14:20:32 +0000

    2 new 2.12 overrides: Gtknotebook::set_group and GtkNotebook::get_group  Because php does not support pointers directly, get_group will not return the actual pointer but instead an integer representation of the pointer (mainly useful for comparison) set_group can take any variable, but can also take a gtknotebook instance - for gtknotebook instances it will internally call set_group(get_group()) so you can easily set multiple notebooks to the same group without keeping your initial variable used for the group hanging around

--- demos/examples/notebook.php
+++ demos/examples/notebook.php
@@ -4,27 +4,40 @@ $window = new GtkWindow();
 $window->set_title('Notebooks and Pages');
 $window->connect_simple('destroy', array('gtk', 'main_quit'));
 $window->add($vbox = new GtkHBox());
+$group = 'MyGroup';
 
-$vbox->add($notebook = new GtkNoteBook());
-$notebook->append_page($label = new GtkLabel('This is the first page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->append_page($label = new GtkLabel('This is the second page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->append_page($label = new GtkLabel('This is the third page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->set_group('Mygroup');
-var_dump($notebook->get_group());
+$vbox->add($notebook1 = new GtkNoteBook());
+$notebook1->append_page($label = new GtkLabel('This is the first page'));
+$notebook1->set_tab_detachable($label, true);
+$notebook1->append_page($label = new GtkLabel('This is the second page'));
+$notebook1->set_tab_detachable($label, true);
+$notebook1->append_page($label = new GtkLabel('This is the third page'));
+$notebook1->set_tab_detachable($label, true);
+$notebook1->set_group($group);
+var_dump($notebook1->get_group());
 
-$vbox->add($notebook = new GtkNoteBook());
-$notebook->append_page($label = new GtkLabel('This is the first page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->append_page($label = new GtkLabel('This is the second page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->append_page($label = new GtkLabel('This is the third page'));
-$notebook->set_tab_detachable($label, true);
-$notebook->set_group('Mygroup');
+$vbox->add($notebook2 = new GtkNoteBook());
+$notebook2->append_page($label = new GtkLabel('This is the first page'));
+$notebook2->set_tab_detachable($label, true);
+$notebook2->append_page($label = new GtkLabel('This is the second page'));
+$notebook2->set_tab_detachable($label, true);
+$notebook2->append_page($label = new GtkLabel('This is the third page'));
+$notebook2->set_tab_detachable($label, true);
+$notebook2->set_group($group);
+var_dump($notebook2->get_group());
 
-var_dump($notebook->get_group());
+unset($group);
+
+$vbox->add($notebook3 = new GtkNoteBook());
+$notebook3->append_page($label = new GtkLabel('This is the first page'));
+$notebook3->set_tab_detachable($label, true);
+$notebook3->append_page($label = new GtkLabel('This is the second page'));
+$notebook3->set_tab_detachable($label, true);
+$notebook3->append_page($label = new GtkLabel('This is the third page'));
+$notebook3->set_tab_detachable($label, true);
+$notebook3->set_group($notebook1);
+var_dump($notebook3->get_group());
+
 $window->show_all();
 Gtk::main();
 ?>
